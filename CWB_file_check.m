1146493864518efbf018713598d7407f8d27c0ed
chemars <37469933+chemars@users.noreply.github.com>
2019-08-06 15:01:48 +0800

    Update CWB_file_check.m

--- CWB_file_check.m
+++ CWB_file_check.m
@@ -4,12 +4,12 @@ for ii = 1:size(file_list,1)
   disp(ii)
   filename = file_list(ii).name;
   fileID = fopen(filename);
-  row1 = textscan(fileID,"#%s %s",1);
-  row2 = textscan(fileID,"#%s %5c %14c",1);
-  row3 = textscan(fileID,"#%s %s",1);
-  row4 = textscan(fileID,"#%s %f",1);
-  row5 = textscan(fileID,"#%s %f",1);
-  row6 = textscan(fileID,"#%s %s %s",1);
+  row1 = textscan(fileID,"%s %s",1);
+  row2 = textscan(fileID,"%s %5c %14c",1);
+  row3 = textscan(fileID,"%s %s",1);
+  row4 = textscan(fileID,"%s %f",1);
+  row5 = textscan(fileID,"%s %f",1);
+  row6 = textscan(fileID,"%s %s %s",1);
   station_code = char(row1{2})
   sample_rate = row5{2}
   unit = char(row6{2});
@@ -19,8 +19,12 @@ for ii = 1:size(file_list,1)
     disp("Unit Error.")
     break;
   endif
-  if isnumeric(sample_rate) == 0
+  if strcmp(char(row5{1}),"#SampleRate(Hz):") == 0
     disp("Sample Rate Error.")
+    break;
+  endif
+  if isnumeric(sample_rate) == 0
+    disp("Sample Rate Number Error.")
     break;
   endif
 endfor
