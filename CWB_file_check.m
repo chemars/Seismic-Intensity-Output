58281af361ad7fb3a58be5e18f959a2c570ce6a9
chemars <37469933+chemars@users.noreply.github.com>
2019-08-07 10:09:02 +0800

    Update CWB_file_check.m

--- CWB_file_check.m
+++ CWB_file_check.m
@@ -3,28 +3,31 @@ file_list = dir("*.*.txt");
 for ii = 1:size(file_list,1)
   disp(ii)
   filename = file_list(ii).name;
-  fileID = fopen(filename);
+  fileID = fopen(filename,"r");
   row1 = textscan(fileID,"%s %s",1);
-  row2 = textscan(fileID,"%s %5c %14c",1);
+  row2 = textscan(fileID,"%s %[^(] %[^\r\n]",1);
   row3 = textscan(fileID,"%s %s",1);
   row4 = textscan(fileID,"%s %f",1);
   row5 = textscan(fileID,"%s %f",1);
   row6 = textscan(fileID,"%s %s %s",1);
   station_code = char(row1{2})
   sample_rate = row5{2}
-  unit = char(row6{2});
   fclose(fileID);
+  
   disp("")
-  if strcmp(unit,"gal.") == 0
-    disp("Unit Error.")
-    break;
-  endif
   if strcmp(char(row5{1}),"#SampleRate(Hz):") == 0
     disp("Sample Rate Error.")
+    disp(file_list(ii).name)
     break;
   endif
   if isnumeric(sample_rate) == 0
     disp("Sample Rate Number Error.")
+    disp(file_list(ii).name)
+    break;
+  endif
+  if strcmp(char(row6{2}),"gal.") == 0
+    disp("Unit Error.")
+    disp(file_list(ii).name)
     break;
   endif
 endfor
