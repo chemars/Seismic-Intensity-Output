1063fb65cf32e1f0e0270461305b4a241910e0f3
chemars <37469933+chemars@users.noreply.github.com>
2019-08-07 10:26:35 +0800

    Update intensity_scale_for_CWB.m

--- intensity_scale_for_CWB.m
+++ intensity_scale_for_CWB.m
@@ -6,19 +6,26 @@ ew_column = 4;
 ud_column = 2;
 file_list = dir("*.*.txt");
 for ii = 1:size(file_list,1)
-  ii
+  disp(ii)
   filename = file_list(ii).name;
-  fileID = fopen(filename);
-  row1 = textscan(fileID,"#%s %s",1);
-  row5 = textscan(fileID,"#%s %f",1,"HeaderLines",4);
-  station_code = char(row1{2})  
-  if strcmp(char(row5{1}),"SampleRate(Hz):") == 0
+  fileID = fopen(filename,"r");
+  row1 = textscan(fileID,"%s %s",1);
+  row5 = textscan(fileID,"%s %f",1,"HeaderLines",3);
+  station_code = char(row1{2})
+  sample_rate = row5{2}
+  fclose(fileID);  
+  disp("")
+  if strcmp(char(row5{1}),"#SampleRate(Hz):") == 0
     disp("Sample Rate Error.")
+    disp(file_list(ii).name)
     break;
   endif
-  sample_rate = row5{2}
-  fclose(fileID);
-  
+  if isnumeric(sample_rate) == 0
+    disp("Sample Rate Number Error.")
+    disp(file_list(ii).name)
+    break;
+  endif
+
   I_cwb2000 = cwb2000(filename,delimiter,ignore_row,ns_column,ew_column,ud_column);
   I_cwb2020 = cwb2020(filename,delimiter,ignore_row,ns_column,ew_column,ud_column,sample_rate);
   I_jma = jma(filename,delimiter,ignore_row,ns_column,ew_column,ud_column,sample_rate);
