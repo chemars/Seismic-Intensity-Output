b861b34f8c55ea736e79f874a628ef55df2df435
chemars <37469933+chemars@users.noreply.github.com>
2019-12-11 20:41:54 +0800

    Update CWB_calculate.m

--- CWB_calculate.m
+++ CWB_calculate.m
@@ -22,16 +22,6 @@ for ii = 1:size(file_list,1)
   sample_rate = row5{2};
   fclose(fileID);
   disp("")
-  if strcmp(char(row5{1}),"#SampleRate(Hz):") == 0
-    disp("Sample Rate Error.")
-    disp(file_list(ii).name)
-    break;
-  endif
-  if isnumeric(sample_rate) == 0
-    disp("Sample Rate Number Error.")
-    disp(file_list(ii).name)
-    break;
-  endif
 
   I_cwb2000 = cwb2000(filename,delimiter,ignore_row,ns_column,ew_column,ud_column);
   CWB_scale = cwb2020(filename,delimiter,ignore_row,ns_column,ew_column,ud_column,sample_rate);
