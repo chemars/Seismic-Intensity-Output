509e9be9ba3f0238ca1f379c7b3a5285ecad2d62
chemars <37469933+chemars@users.noreply.github.com>
2019-12-12 17:39:11 +0800

    Update CIT_check.m

--- CIT_check.m
+++ CIT_check.m
@@ -8,21 +8,21 @@ for ii = 1:size(file_list,1)
   filename = file_list(ii).name;
   fileID = fopen(filename,"r");
   row8 = textscan(fileID,"%s %s",1,"HeaderLines",7);
-  row9 = textscan(fileID,"%s %s",1);
-  row10 = textscan(fileID,"%s %[^\r\n]",1);
-  row11 = textscan(fileID,"%s %f",1);
-  row12 = textscan(fileID,"%s %f",1);
-  row13 = textscan(fileID,"%s %[^\r\n]",1);
-  row14 = textscan(fileID,"%s %s",1);
-  row15 = textscan(fileID,"%s %f",1);
-  row16 = textscan(fileID,"%s %f",1);
-  row17 = textscan(fileID,"%s %s %s",1);
+  row9 = textscan(fileID,"%[^:] %*s %s",1);
+  row10 = textscan(fileID,"%[^:] %*s %[^\r\n]",1);
+  row11 = textscan(fileID,"%[^:] %*s %f",1);
+  row12 = textscan(fileID,"%[^:] %*s %f",1);
+  row13 = textscan(fileID,"%[^:] %*s %[^\r\n]",1);
+  row14 = textscan(fileID,"%[^:] %*s %s",1);
+  row15 = textscan(fileID,"%[^:] %*s %f",1);
+  row16 = textscan(fileID,"%[^:] %*s %f",1);
+  row17 = textscan(fileID,"%[^:] %*s %[^\r\n]",1);
   station_code = char(row9{2})
   sample_rate = row16{2}
   fclose(fileID);
   
   disp("")
-  if strcmp(char(row16{1}),"#SampleRate(Hz):") == 0
+  if strncmp(char(row16{1}),"#SampleRate(Hz)",15) == 0
     disp("Sample Rate Error.")
     disp(file_list(ii).name)
     break;
@@ -32,7 +32,7 @@ for ii = 1:size(file_list,1)
     disp(file_list(ii).name)
     break;
   endif
-  if strcmp(char(row17{2}),"gal.") == 0
+  if strncmp(char(row17{2}),"gal.",4) == 0
     disp("Unit Error.")
     disp(file_list(ii).name)
     break;
